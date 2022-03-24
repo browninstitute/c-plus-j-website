46e3a069852278d79982719481bb5a4b1baf4da0
Juan Francisco Saldarriaga <juanfrans@gmail.com>
2022-03-24 17:25:42 -0400

    updated readme and eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-//   eleventyConfig.addPassthroughCopy("**/*.png");
-//   eleventyConfig.addPassthroughCopy("**/*.css");
+  eleventyConfig.addPassthroughCopy("**/*.jpg");
+  eleventyConfig.addPassthroughCopy("**/*.jpeg");
+  eleventyConfig.addPassthroughCopy("**/*.png");
+  eleventyConfig.addPassthroughCopy("**/*.css");
 };
