44ea7a8d626457a3559756078fc4fa74eea6eb6b
Juan Francisco Saldarriaga <juanfrans@gmail.com>
2022-03-25 14:08:12 -0400

    fixed passthrough problem on local host

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,19 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.setTemplateFormats([
+    "md",
+    "css",
+    "jpg",
+    "png",
+    "ico"
+  ]);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPassthroughCopy("**/*.jpg");
-  eleventyConfig.addPassthroughCopy("**/*.jpeg");
-  eleventyConfig.addPassthroughCopy("**/*.png");
-  eleventyConfig.addPassthroughCopy("**/*.css");
+  // eleventyConfig.addPassthroughCopy("images");
+  // eleventyConfig.addPassthroughCopy("css");
+  // eleventyConfig.addPassthroughCopy("**/*.jpg");
+  // eleventyConfig.addPassthroughCopy("**/*.jpeg");
+  // eleventyConfig.addPassthroughCopy("**/*.png");
+  // eleventyConfig.addPassthroughCopy("**/*.css");
 };
+
